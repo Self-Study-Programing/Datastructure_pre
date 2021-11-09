7b8a10c486560811ae7cff045d72396314aece1c
강성훈 <80798929+ksh5324@users.noreply.github.com>
2021-11-09 10:30:24 +0900

    study: dfs

---
+++ python/dfs.py
@@ -0,0 +1,21 @@
+def dfs(v):
+    visited[v] = 1
+    print(v, end=' ')
+    for i in graph[v]:
+        if not visited[i]: # not === !
+            dfs(i)
+
+graph = [
+    [],
+    [2,3,8],
+    [1,7],
+    [1,4,5],
+    [3,5],
+    [3,4],
+    [7],
+    [2,6,8],
+    [1,7]
+]
+
+visited = [0] * 9
+dfs(1)
